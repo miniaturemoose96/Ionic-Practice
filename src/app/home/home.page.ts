b9c13621044db72f399033b81b83b73740f13544
roberto sanchez <roberto@urbantxt.com>
2020-07-22 15:52:20 -0700

    added reactive form

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+interface Journal { 
+  id: Number;
+  title: String;
+  decrip: String;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,27 +13,37 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  // Journal structure
-  journal = {
-    id: Number,
-    title: String,
-    decrip: String
-  };
-
   // variables used 
-  listOfJournals = [{id: '1',title: 'Note1', decrip: 'Description'},
-                    {id: '2',title: 'Note2', decrip: 'Description2'},
-                    {id: '3',title: 'Note3', decrip: 'Description3'},
-                    {id: '4',title: 'Note4', decrip: 'Description4'},
-                    {id: '5',title: 'Note5', decrip: 'Description5'},
-                    {id: '6',title: 'Note6', decrip: 'Description6'},
-                    {id: '7',title: 'Note7', decrip: 'Description7'},
-                    {id: '8',title: 'Note8', decrip: 'Description8'},
-                    {id: '9',title: 'Note9', decrip: 'Description9'},
-                    {id: '10',title: 'Note10', decrip: 'Description10'}];
+  listOfJournals = [];
 
-  constructor() {}
+  journalForm: FormGroup;
 
-  // List all note titles
+  constructor(public fb: FormBuilder) {
+    this.initForm();
+  }
+
+  // init the form object - controls
+  initForm(){
+    this.journalForm = this.fb.group({
+      id: [null, Validators.required],
+      title: [null, Validators.required],
+      decrip: [null, Validators.required],
+    })
+  }
+
+  // process form
+  onFormSubmit(){
+      console.log(this.journalForm.value);
+
+      /*
+        TODO:
+          logic needed to send form data to server.
+      */
+
+
+      // add journals to the array 
+      this.listOfJournals.push(this.journalForm.value);
+      this.journalForm.reset();
+  }
   
 }

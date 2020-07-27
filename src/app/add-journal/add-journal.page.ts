645f36f1dfa1a5c13be844122ac055c43d6a6bb5
Alejandro <alejandro.bernal@urbantxt.com>
2020-07-27 15:27:33 -0700

    Successfully creates a new journal when the user adds it.

--- src/app/add-journal/add-journal.page.ts
+++ src/app/add-journal/add-journal.page.ts
@@ -3,6 +3,11 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { DataService } from '../shared/data.service';
 import { Router } from '@angular/router';
 
+interface ServerResponse {
+  message: string;
+  status: number;
+  data: any;
+}
 @Component({
   selector: 'app-add-journal',
   templateUrl: './add-journal.page.html',
@@ -30,15 +35,21 @@ export class AddJournalPage implements OnInit {
 
   // process form
   onFormSubmit() {
-    console.log(this.journalForm.value);
+    // 1. form validation
 
-    /*
-      TODO:
-        logic needed to send form data to server.
-    */
-    this._dataService.updateObs(this.journalForm.value);
-    this.journalForm.reset();
-    this.router.navigate(['/home']);
+
+    //2. making the request
+    this._dataService.addJournal(this.journalForm.value)
+    .subscribe((serverRes: ServerResponse) => {
+      // deals with status 200
+      this._dataService.updateObs(serverRes.data);
+      this.journalForm.reset();
+      this.router.navigate(['/home']);
+    }, (err) => {
+      // deals with errors
+      console.log(err);
+      alert('server error');
+    })
   }
 
 }
--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
@@ -8,13 +8,31 @@ export class DataService {
   private journalListState = new BehaviorSubject([]);
   public journalObs = this.journalListState.asObservable();
 
-  constructor() { }
+  constructor(public http: HttpClient) {
+    this.getJournals().subscribe(
+      (data: any) => {
+        console.log(data);
+        this.journalListState.next(data);
+      },
+      (err) => {
+        console.log(err);
+      }
+    )
+  }
 
-  updateObs(journal){
-      const newList = this.journalListState.value;
+  updateObs(journal) {
+    const newList = this.journalListState.value;
 
-      newList.push(journal);
-  
-      this.journalListState.next(newList);
+    newList.push(journal);
+
+    this.journalListState.next(newList);
+  }
+
+  getJournals() {
+    return this.http.get('http://localhost:8888/view-journals');
+  }
+
+  addJournal(newJournal) {
+    return this.http.post('http://localhost:8888/add-journal', newJournal);
   }
 }
